6512af6532dbd1a616374ed72269bbbf4f242d8e
Leonardo Camara <leonardo.camara@compasso.com.br>
2021-03-08 17:44:58 -0300

    First module

---
+++ app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,16 @@
+class NegociacaoController {
+
+    private _inputData;
+    private _inputQuantidade;
+    private _inputValor;
+
+    constructor() {
+        this._inputData = document.querySelector('#data')
+        this._inputQuantidade = document.querySelector('#quantidade')
+        this._inputValor = document.querySelector('#valor')
+    }
+
+    adiciona(event) {
+        alert('minha logica aqui');
+    }
+}
